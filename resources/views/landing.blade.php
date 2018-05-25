bc267602b294fdd685d0dc5e6474f57c105e33a2
Unknown <michaelfami@gmail.com>
2018-05-25 12:59:30 -0500

    added more content

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,6 +5,36 @@
         <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
     </div>
 </div>
+<div id="threeButtons" class="row mx-5">
+    <div class="col">
+        <button id="onlineCourseBtn" class="btn px-4 py-2" type="button" name="button">ONLINE COURSES CATALOG</button>
+    </div>
+
+    <div class="w-25">
+        <button id="contactButton" class="btn px-4 py-2 float-left" type="button " name="button">CONTACT</button>
+        <button id="applyButton" class="btn btn-primary float-right px-4 py-2" type="button" name="button">APPLY</button>
+    </div>
+
+</div>
+
+<div class="row my-5 py-5">
+    <div class="col-6">
+
+    </div>
+    <div class="col-6 text-left pr-4">
+        <h1 class="text-primary">Our Online Structure</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
+<div class="row my-4 py-4 px-5">
+    <div class="col-6 text-right pl-4 ml-4">
+        <h1 class="text-primary">The Experience</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-6">
+
+    </div>
+</div>
 
 <div class="row justify-content-center">
     <div class="slickSlides">
@@ -13,6 +43,20 @@
         <div>your content</div>
     </div>
 
+    <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
+        <div>
+            <h3>1</h3></div>
+        <div>
+            <h3>2</h3></div>
+        <div>
+            <h3>3</h3></div>
+        <div>
+            <h3>4</h3></div>
+        <div>
+            <h3>5</h3></div>
+        <div>
+            <h3>6</h3></div>
+    </div>
 
 
 </div>
